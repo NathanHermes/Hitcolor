1b26734bf7d5a58e38d6174658aa1c1d1e49f379
Nathan Hermes <hermesnathan8@gmail.com>
2025-07-07 23:35:42 -0300

    feat: :sparkles: add header components in layout of site

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './global.css'
 
 import { Home } from './Home'
+import { Header } from './components/header'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Header />
     <Home />
-  </StrictMode>,
+  </StrictMode>
 )
