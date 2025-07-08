e6ecbcaf3541cf879173b44beef8a1be3fd07de4
Nathan Hermes <hermesnathan8@gmail.com>
2025-07-07 23:30:20 -0300

    chore: :wrench: add alias to folders

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@assets': '/src/assets',
+    }
+  },
   plugins: [
     react(),
     tailwindcss()
