8b1873f1685e5f7ec7bfda692f077080ed0b597b
NathanTriplos <nathan.hermes@triplos.com.br>
2025-07-16 23:30:40 -0300

    feat: update router by react to router by react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './global.css'
+import { createRoot } from 'react-dom/client';
+import './global.css';
 
-import { Home } from './Home'
-import { Header } from './components/header'
+import { BrowserRouter } from 'react-router';
+import { Header } from './components';
+import { Home } from './Home';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <BrowserRouter>
     <Header />
     <Home />
-  </StrictMode>
+  </BrowserRouter>
 )
