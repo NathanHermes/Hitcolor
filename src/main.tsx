eda64fce2d18c50f256fe83a20a3332dfe34dda5
NathanTriplos <nathan.hermes@triplos.com.br>
2025-07-16 23:37:22 -0300

    fix: update imports by absolute paths to aliases

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import './global.css';
 
+import { Header } from '@components';
 import { BrowserRouter } from 'react-router';
-import { Header } from './components';
-import { Home } from './Home';
+import { Home } from './home';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
