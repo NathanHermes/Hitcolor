ae74f57ba79c2d8cdd2f8a105b01b5769405d725
NathanTriplos <nathan.hermes@triplos.com.br>
2025-07-16 23:21:58 -0300

    chore: add new aliases to path of project folders

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@': '/src',
-      '@assets': '/src/assets',
-      '@components': '/src/components',
+      "@": "./src",
+      "@assets": "./src/assets",
+      "@components": "./src/components",
     }
   },
   plugins: [
